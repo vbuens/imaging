4bfe3eb328c99c830381f20e65a8715c6773fce9
Vanessa Bueno Sancho <vbuens@gmail.com>
2019-04-23 14:09:27 +0100

    Included comments

--- countdotspores.py
+++ countdotspores.py
@@ -15,12 +15,14 @@ from skimage import measure
 
 os.chdir('/Users/buenov/Documents/PhD/Spores/counts')
 
-
+#Get the image file as an argument
 image_title=sys.argv[1] 
 im = io.imread('%s'%image_title, as_grey=True)
+# Get the number of spores
 labels = measure.label(im)
 print(labels.max())
 plt.imshow(im, cmap='gray')
+#Use the name of the image as title for the output
 title=image_title.split('.')[0]
 plt.title('Number of spores: %s' %labels.max())
 plt.savefig('results/%s_%s.png'%(title,labels.max()))
