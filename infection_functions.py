841827bc7f360b4ccd7068c6f5d171a67b1cf6b0
Vanessa Bueno Sancho <vbuens@gmail.com>
2019-05-01 13:51:34 +0100

    Estimate percentage of (wheat rusts) infection in leaves from images

---
+++ infection_functions.py
@@ -0,0 +1,106 @@
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import argparse
+import utils
+import cv2
+import numpy as np
+import webcolors
+
+# Functions:
+def get_image(image_path):
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
+def closest_colour(requested_colour):
+    min_colours = {}
+    for key, name in webcolors.css3_hex_to_names.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - requested_colour[0]) ** 2
+        gd = (g_c - requested_colour[1]) ** 2
+        bd = (b_c - requested_colour[2]) ** 2
+        min_colours[(rd + gd + bd)] = name
+    return min_colours[min(min_colours.keys())]
+def color_name(center_colours):
+    colornames=[]
+    for i in range(0,len(center_colours)):
+        colornames.append(closest_colour((center_colours[i][0],center_colours[i][1],center_colours[i][2])))
+    return colornames
+
+def print_image(imagefile,image,center_colours,labels,k,output_dir):
+    center_colours = np.uint8(center_colours)
+    res = center_colours[labels.flatten()]
+    #print(len(center_colours))
+    res2 = res.reshape((image.shape))
+    #cv2.imshow('res2',res2)
+    plt.imshow(res2)
+    plt.title(imagefile.split('.')[0])
+    plt.savefig('%s/standard_%s_%i_kmeans.jpg' % (output_dir,imagefile.split('.')[0],k))
+    plt.clf()
+
+## Getting the healthy and infected thresholds
+
+def get_threshold_values(healthy,infected,k,output_dir):
+    imagefiles = [healthy, infected] #,"mid.jpg"]
+    plt.rcParams['figure.figsize'] = [14, 7]
+
+    for imagefile in imagefiles:
+        image = get_image(imagefile)
+        Z = image.reshape((-1,3))
+        # convert to np.float32
+        Z = np.float32(Z)
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+        K = k #number of colours
+        ret,labels,center_colours=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+
+        print_image(imagefile,image,center_colours,labels,K,output_dir)
+
+        counts={}
+        perc={}
+        for i in range(0,K):
+            counts[i]=list(labels.flatten()).count(i)
+            perc[i]=round((list(labels.flatten()).count(i)/len(labels))*100,2)
+        colorlabels=color_name(center_colours)
+        # We get ordered colors by iterating through the keys
+        ordered_colors = [center_colours[i]/255 for i in counts.keys()]
+
+        # Get rid of the background and get the values for colour green (healthy) and khaki (infected)
+        blackindex=colorlabels.index('black')
+        green = [i for i, s in enumerate(colorlabels) if 'green' in s]
+        khaki = [i for i, s in enumerate(colorlabels) if 'khaki' in s]
+
+        if blackindex in counts:
+            counts.pop(blackindex)
+        if blackindex in perc:
+            perc.pop(blackindex)
+            ordered_colors.pop(blackindex)
+
+        colorlabels.pop(blackindex)
+        colornames=[]
+        for colour in center_colours:
+            colornames.append(closest_colour((colour[0],colour[1],colour[2])))
+        colornames.pop(blackindex)
+        list(center_colours).pop(blackindex)
+
+        newpercs=[round((i/sum(perc.values()))*100,2) for i in perc.values()]
+
+
+
+        if imagefile == imagefiles[0]:
+            healthy_values=[center_colours[green[0]][0],center_colours[green[0]][1],center_colours[green[0]][2]]
+            healthy_pie = [counts.values(),colorlabels,ordered_colors]
+            #print("Healthy values: %s" %healthy_values)
+
+        if imagefile == imagefiles[1]:
+            inf_values=[center_colours[khaki[0]][0],center_colours[khaki[0]][1],center_colours[khaki[0]][2]]
+            inf_pie = [counts.values(),colorlabels,ordered_colors]
+            #print("Inf values: %s" % inf_values)
+
+    plt.subplot(121)
+    plt.pie(healthy_pie[0],labels=healthy_pie[1],colors=healthy_pie[2])
+    plt.title(healthy.split(".")[0])
+    plt.subplot(122)
+    plt.pie(inf_pie[0],labels=inf_pie[1],colors=inf_pie[2])
+    plt.title(infected.split(".")[0])
+    plt.savefig('%s/standards_%i_pie.jpg' % (output_dir,K))
+    plt.clf()
+    return healthy_values, inf_values
---
+++ perc_infection.py
@@ -0,0 +1,143 @@
+import os
+import sys
+import argparse
+import utils
+import cv2
+import glob
+import webcolors
+import numpy as np
+import matplotlib.pyplot as plt
+from infection_functions import get_image,color_name,print_image, get_threshold_values
+
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--k', type=int, nargs='?', help='output directory for results (default=5)',default=5)
+parser.add_argument('-o','--outputdir', type=str, help='output directory for results (default=results)',default="results")
+parser.add_argument('-i','--inputdir', required= True, type=str, help='directory where the images to analyse are found')
+parser.add_argument('-f','--format', type=str, help='format of the pictures (default=.tif)', default=".tif")
+parser.add_argument('-p','--positivecontrol', required= True, type=str, help='Image file of an infected leaf')
+parser.add_argument('-n','--negativecontrol', required= True, type=str, help='Image file of a healthy leaf')
+args = parser.parse_args()
+output_dir = args.outputdir
+output_file = str(output_dir)+"/infection_percentages.csv"
+input_dir = args.inputdir
+format = args.format
+healthy = args.negativecontrol
+infected = args.positivecontrol
+K = args.k
+
+for file in [healthy,infected]:
+    try:
+        open(file, 'r')
+    except FileNotFoundError:
+        parser.error("File %s not found" % file)
+if os.path.exists(input_dir) == 'False':
+    parser.error("Directory %s not found" % input_dir)
+if os.path.exists(output_dir) == 'False':
+    os.mkdir(output_dir)
+#sys.stdout = open('stdout.txt', 'w')
+print('''
+Output directory : %s
+Input directory : %s
+Images format : %s
+Positive control (infected) image : %s
+Negative control (healthy) image : %s
+Number of clusters to find infection (k) : %i
+Final output: %s
+''' % (output_dir,input_dir,format,healthy,infected,K,output_file))
+
+healthy_values,inf_values = get_threshold_values(healthy,infected,K,output_dir)
+print("Healhy values: %s \nInfected values: %s" % (str(healthy_values),str(inf_values)))
+
+output = open(output_file, "a")
+
+for imagefile in glob.glob(str(input_dir)+"/*"+str(format)):
+    image = get_image(imagefile)
+    print("- File : %s " % imagefile)
+    Z = image.reshape((-1,3))
+    # convert to np.float32
+    Z = np.float32(Z)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    K = 5    #number of colours
+    ret,labels,center_colours=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+    # Now convert back into uint8, and make original image
+    center_colours = np.uint8(center_colours)
+    res = center_colours[labels.flatten()]
+    res2 = res.reshape((image.shape))
+    plt.imshow(res2)
+    plt.savefig('%s/%s_%i_kmeans.jpg' % (output_dir,imagefile.split('.')[0].split('/')[1],K))
+    plt.clf()
+
+    counts={}
+    perc={}
+    for i in range(0,K):
+        counts[i]=list(labels.flatten()).count(i)
+        perc[i]=round((list(labels.flatten()).count(i)/len(labels))*100,2)
+    colorlabels=color_name(center_colours)
+
+    # We get ordered colors by iterating through the keys
+    ordered_colors = [center_colours[i]/255 for i in counts.keys()]
+    plt.pie(counts.values(),labels=colorlabels,colors=ordered_colors)
+    plt.savefig('%s/%s_%i_pie_black.jpg' % (output_dir,imagefile.split('.')[0].split('/')[1],K))
+    plt.show()
+    plt.clf()
+
+    blackindex=colorlabels.index('black')
+    if blackindex in counts:
+        counts.pop(blackindex)
+    if blackindex in perc:
+        perc.pop(blackindex)
+        ordered_colors.pop(blackindex)
+
+    colorlabels.pop(blackindex)
+    colornames=[]
+    for colour in center_colours:
+        colornames.append(closest_colour((colour[0],colour[1],colour[2])))
+    colornames.pop(blackindex)
+    list(center_colours).pop(blackindex)
+    newpercs=[round((i/sum(perc.values()))*100,2) for i in perc.values()]
+
+    plt.pie(counts.values(),labels=colorlabels,colors=ordered_colors)
+    plt.savefig('%s/%s_%i_pie.jpg' % (output_dir,imagefile.split('.')[0].split('/')[1],K))
+    plt.title(imagefile.split(".")[0])
+    plt.show()
+    plt.clf()
+
+    ### GET COLOURS
+    colors={}
+    for i in range(0,len(center_colours)):
+    #    print("HEALTHY, INF \n %s, %s" % (healthy_values,inf_values))
+        rc=center_colours[i][0] ; gc=center_colours[i][1] ; bc=center_colours[i][2]
+        rd=(healthy_values[0]-rc)**2
+        gd=(healthy_values[1]-gc)**2
+        bd=(healthy_values[2]-bc)**2
+        healthy_rgb=rd + gd + bd
+        rd_inf=(inf_values[0]-rc)**2
+        gd_inf=(inf_values[1]-gc)**2
+        bd_inf=(inf_values[2]-bc)**2
+        inf_rgb=rd_inf + gd_inf + bd_inf
+        colors[color_name(center_colours)[i]]=[healthy_rgb,inf_rgb]
+#        print(colors[color_name(center_colours)[i]].index(min(colors[color_name(center_colours)[i]])))
+
+
+    color_name(center_colours)
+    ## WITH 5 CLUSTERS : 16.47 OF INFECTION
+    percinf=0
+    totalpercs=sum(newpercs)
+    count=0
+    if 'black' in colors:
+        colors.pop('black')
+#    print(center_colours)
+#    print(colors.keys())
+    for color in colors.keys():
+        if colors[color].index(min(colors[color])) == 0 :
+            print("Healthy: %s" % color)
+        else:
+            print("Infected: %s" % color)
+            percinf+=newpercs[count]
+        count+=1
+
+    print(percinf)
+    output.write("\n%s,%f" % (imagefile.split('.')[0].split('/')[1],percinf))
+
+output.close()
